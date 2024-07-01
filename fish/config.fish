c1ec5b8f2b1a1dc72164a4254941bf966bbdb915
ryuryu-ymj <33318253+ryuryu-ymj@users.noreply.github.com>
2024-07-01 18:57:28 +0900

    Delete fish startup choice of tmux sessions.

--- fish/config.fish
+++ fish/config.fish
@@ -7,32 +7,4 @@ if status is-interactive
   alias la='ls -a'
 
   starship init fish | source
-
-  if test -z $TMUX
-    set count 0
-    for ses in (tmux list-sessions | cut -d: -f1)
-      if test $ses -eq $count
-        set count (math $count + 1)
-      else if test $ses -gt $count
-        tmux rename-session -t $ses $count
-        set count (math $count + 1)
-      end
-    end
-
-    set ses (tmux list-sessions)
-    if test -z $ses[1]
-      exec tmux new-session
-    else
-      set new 'Create new session'
-      set ses $ses $new
-      set ses (printf '%s\n' $ses | fzf | cut -d: -f1)
-      if test -n "$ses"
-        if test $ses = $new
-          exec tmux new-session -s $count
-        else
-          exec tmux attach-session -t $ses
-        end
-      end
-    end
-  end
 end
